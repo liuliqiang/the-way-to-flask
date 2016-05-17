82f7d2dd444821d1044a45738ec9e59a8bf19ce9
liqiang <liqiang.liu@imaygou.com>
2016-05-17 10:24:00 +0800

    fix: continue debug unfinish code

--- code/app.py
+++ code/app.py
@@ -8,53 +8,67 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def query_records():
+    name = request.args.get('name')
+    print name
     with open('/tmp/data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
-        return jsonify(records)
+        for record in records:
+            if record['name'] == name:
+                return jsonify(record)
+        return jsonify({'error': 'data not found'})
 
 
 @app.route('/', methods=['PUT'])
 def create_record():
     record = json.loads(request.data)
-    with open('/tmp/data.txt', 'wr') as f:
+    with open('/tmp/data.txt', 'r') as f:
         data = f.read()
-        if not data:
-            records = [record]
-        else:
-            records = json.loads(data)
-            records.append(record)
-        f.write(json.dumps(data))
-        return jsonify(records)
+
+    if not data:
+        records = [record]
+    else:
+        records = json.loads(data)
+        records.append(record)
+
+    with open('/tmp/data.txt', 'w') as f:
+        f.write(json.dumps(records, indent=2))
+    return jsonify(record)
 
 
 @app.route('/', methods=['POST'])
 def update_record():
     record = json.loads(request.data)
     new_records = []
-    with open('/tmp/data.txt', 'wr') as f:
+    with open('/tmp/data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
-        for r in records:
-            if r['name'] == record['name']:
-                r['email'] = record['email']
-            new_records.append(r)
-        f.write(json.dumps(new_records))
-    return jsonify(new_records)
+
+    for r in records:
+        if r['name'] == record['name']:
+            r['email'] = record['email']
+        new_records.append(r)
+
+    with open('/tmp/data.txt', 'w') as f:
+        f.write(json.dumps(new_records, indent=2))
+    return jsonify(record)
 
 
 @app.route('/', methods=['DELETE'])
 def delte_record():
     record = json.loads(request.data)
     new_records = []
-    with open('/tmp/data.txt', 'wr') as f:
+    with open('/tmp/data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
         for r in records:
             if r['name'] == record['name']:
                 continue
             new_records.append(r)
-        f.write(json.dumps(new_records))
-    return jsonify(new_records)
 
-app.run()
+    with open('/tmp/data.txt', 'w') as f:
+        f.write(json.dumps(new_records, indent=2))
+
+    return jsonify(record)
+
+app.run(debug=True)
