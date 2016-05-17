829a1273383803b3c0f5827eb8521a26f3893a42
tyrael <liqianglau@outlook.com>
2016-05-18 02:05:13 +0800

    feat: add flask-mongoengine dependence

--- code/app.py
+++ code/app.py
@@ -2,73 +2,70 @@
 # encoding: utf-8
 import json
 from flask import Flask, request, jsonify
+from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'the_way_to_flask',
+    'host': 'localhost',
+    'port': 27017
+}
+
+db = MongoEngine()
+db.init_app(app)
+
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+
+    def to_json(self):
+        return {"name": self.name,
+                "email": self.email}
 
 
 @app.route('/', methods=['GET'])
 def query_records():
     name = request.args.get('name')
-    print name
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-        for record in records:
-            if record['name'] == name:
-                return jsonify(record)
+    user = User.objects(name=name).first()
+
+    if not user:
         return jsonify({'error': 'data not found'})
+    else:
+        return jsonify(user.to_json())
 
 
 @app.route('/', methods=['PUT'])
 def create_record():
     record = json.loads(request.data)
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-
-    if not data:
-        records = [record]
-    else:
-        records = json.loads(data)
-        records.append(record)
-
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(records, indent=2))
-    return jsonify(record)
+    user = User(name=record['name'],
+                email=record['email'])
+    user.save()
+    return jsonify(user.to_json())
 
 
 @app.route('/', methods=['POST'])
 def update_record():
     record = json.loads(request.data)
-    new_records = []
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-
-    for r in records:
-        if r['name'] == record['name']:
-            r['email'] = record['email']
-        new_records.append(r)
-
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(new_records, indent=2))
-    return jsonify(record)
+    user = User.objects(name=record['name']).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        user.update(email=record['email'])
+    return jsonify(user.to_json())
 
 
 @app.route('/', methods=['DELETE'])
 def delte_record():
     record = json.loads(request.data)
-    new_records = []
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-        for r in records:
-            if r['name'] == record['name']:
-                continue
-            new_records.append(r)
+    user = User.objects(name=record['name']).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        user.delete()
+    return jsonify(user.to_json())
 
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(new_records, indent=2))
 
-    return jsonify(record)
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
